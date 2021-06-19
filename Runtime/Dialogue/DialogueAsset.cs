36e7892d06c992fe465c163be701d9dc3ba4e293
olivecrow <olivecrow.games@gmail.com>
2021-06-20 00:35:37 +0900

    원인 모를 버그로 작성중이던거 전부 증발해버리고 다시 작성중. 휴지통으로 이동한 것도 아니고 무슨 Temp폴더에 들어있던것 처럼 전부 없어져버렸다. 개빡치네

---
+++ Runtime/Dialogue/DialogueAsset.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OcDialogue
+{
+    [CreateAssetMenu(fileName = "Dialogue Asset", menuName = "Oc Dialogue/Dialogue Asset")]
+    public class DialogueAsset : ScriptableObject
+    {
+        
+    }
+}
